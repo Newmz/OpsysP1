afb5c189c82b87bacabf26cd3c5e001d4b72d6d2
Timothy Newman <tnewman995@hotmail.com>
2016-10-06 15:29:27 -0400

    added output function and correct error messages

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 import os
 import time
+import queue
+
+DB = 1
 
 class process:
 	def __init__(self,name,at,cbt,nb,io):
@@ -47,46 +50,86 @@ class process:
 	def completionTime(self):
 		return self.completedTime
 
+	def __lt__(self,other):
+		return self.arrivalTime < other.arrivalTime
+
 
 
 def processFile(fn):
+	#this function takes a filename and returns a list of processes parsed from the file, sorted by arrival time
 	processes = []
 	try:
 		f = open(fn)
 	except OSError:
-		#print("ERROR: file did not open correctly. perhaps the filename was incorrect?")
-		sys.exit("ERROR: file did not open correctly. Exiting...")
-	i = 0
+		#file did not open correctly
+		sys.stderr.write("ERROR: invalid input file format\n")
+		sys.exit()
+	i = 0 #line number
 	for line in f:
 		i += 1
 		if line.startswith('#'):
+			#ignore comments
 			continue
 		elif line.strip() == '':
+			#ignore empty lines
 			continue
 		else:
+			#split a valid line by the '|' delimiter and create a process from it
 			line = line.split('|')
 			if len(line) != 5:
-				sys.exit("ERROR: invalid file format: line " + str(i) + " does not have the correct number of arguments. Exiting...")
+				if DB == 1:
+					sys.exit("ERROR: invalid input file format: line " + str(i) + " does not have the correct number of arguments. Exiting...")
+				else:
+					sys.stderr.write("ERROR: invalid input file format\n")
+					sys.exit()
 			processes.append(process(line[0], line[1], line[2], line[3], line[4]))
 			print(processes[len(processes)-1])
+	sorted(processes, key=lambda p:p.getArrivalTime())
 	return processes
 
+def statsOutput(statList, of, appending=False):
+	if appending:
+		f = open(of, 'a')
+	else:
+		f = open(of, 'w')
+	f.write("Algorithm " + statList[0] + "\n")
+	f.write("-- average CPU burst time: " + str(statList[1]) + " ms\n")
+	f.write("-- average wait time: " + str(statList[2]) + " ms\n")
+	f.write("-- average turnaround time: " + str(statList[3]) + " ms\n")
+	f.write("-- total number of context switches: " + str(statList[4]) + "\n")
+	f.write("-- total number of preemptions: " + str(statList[5]) + "\n")
 
 
-def RoundRobin():
-	return 0;
+def RoundRobin(processList):
+	#given a process list, do the RR algorithm and return the 5 needed output stats
+	return ["RR",AvgCPUBurst, AvgWait,AvgTurnaround,numContextSwitches,numPreemptions];
 
-def FCFS():
-	return 0;
+def FCFS(processList):
+	#given a process list, do the FCFS algorithm and return the 5 needed output stats
+	#create FIFO queue
+	processQueue = queue.Queue()
+	return ["FCFS",AvgCPUBurst, AvgWait,AvgTurnaround,numContextSwitches,numPreemptions];
 
-def SJF():
-	return 0;
+def SJF(processList):
+	#given a process list, do the SJF algorithm and return the 5 needed output stats
+	return ["SJF",AvgCPUBurst, AvgWait,AvgTurnaround,numContextSwitches,numPreemptions];
 
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		sys.exit("ERROR: missing command-line arguments.\n\tformat:\tpython main.py <filename>")
-	processList = processFile(sys.argv[1])
+	if len(sys.argv) != 3:
+		sys.exit("ERROR: Invalid arguments\nUSAGE: python main.py <input-file> <stats-output-file>")
+	processList = processFile(sys.argv[1]) 
+	print("\n\n\n")
+	for i in processList:
+		print(i)
+	statsOutput(["Test",1,2,3,4,5], sys.argv[2])
+
+# -- average CPU burst time: ###.## ms
+# -- average wait time: ###.## ms
+# -- average turnaround time: ###.## ms
+# -- total number of context switches: ##
+# -- total number of preemptions: ##
+
 
 
 
